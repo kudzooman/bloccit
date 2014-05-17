8d18df36087eb9aebad17d13046695738e4fc582
kudzooman <kudzooman@gmail.com>
2014-05-17 07:42:50 -0700

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://iota1talk.herokuapp.com
+config.action_mailer.default_url_options = { host: 'iota1talk.herokuapp.com' }
 end
