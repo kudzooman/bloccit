dd2f260d690af9e9b40d929b9ef588f648ea4bde
kudzooman <kudzooman@gmail.com>
2014-05-24 08:50:46 -0700

    Implemented voting

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
+      get '/up-vote' => 'votes#up_vote', as: :up_vote
+      get '/down-vote' => 'votes#down_vote', as: :down_vote
     end
   end
 
