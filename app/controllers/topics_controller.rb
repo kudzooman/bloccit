8de2daa0fdb2ad71a9c3639701a3d425fd39eadd
kudzooman <kudzooman@gmail.com>
2014-05-25 21:34:12 -0700

    Scoped topics for privacy

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
-    @topics = Topic.paginate(page: params[:page], per_page: 15) 
+    @topics = Topic.visible_to(current_user).paginate(page: params[:page], per_page: 15) 
     #changed page_per to 15 instead of 10
     authorize @topics
   end
@@ -12,6 +12,7 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+    authorize @topic
     @posts = @topic.posts.paginate(page: params[:page], per_page: 15)
     #changed page_per to 15 instead of 10
   end
